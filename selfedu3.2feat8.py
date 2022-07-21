a36e234ab8df63af1fa85e36fd8904149659e380
San4ouss <a1998alex98@mail.ru>
2022-07-21 14:15:24 +0300

    new task 3.2 feat 8

--- selfedu3.2feat8.py
+++ selfedu3.2feat8.py
@@ -4,6 +4,47 @@ class Handler:
 
     def __call__(self, func):
         def wrapper(request, *args, **kwargs):
-            pass
+            if request.get('method') in self.__methods or request.get('method') is None:
+                return self.post(func, request) if request.get('method') == "POST" else self.get(func, request)
+            else:
+                return None
 
         return wrapper
+
+    def get(self, func, request, *args, **kwargs):
+        return f"GET: {func(request)}"
+
+    def post(self, func, request, *args, **kwargs):
+        return f"POST: {func(request)}"
+
+
+# @Handler(methods=('GET', 'POST'))
+# def contact(request):
+#     return "контакты"
+#
+#
+# res = contact({"method": "POST", "url": "contact.html"})
+# print(res)
+
+assert hasattr(Handler, 'get') and hasattr(Handler, 'post'), "класс Handler должен содержать методы get и post"
+
+
+@Handler(methods=('GET', 'POST'))
+def contact2(request):
+    return "контакты"
+
+
+assert contact2({"method": "POST"}) == "POST: контакты", "декорированная функция вернула неверные данные"
+assert contact2({"method": "GET"}) == "GET: контакты", "декорированная функция вернула неверные данные"
+assert contact2({"method": "DELETE"}) is None, "декорированная функция вернула неверные данные"
+assert contact2({}) == "GET: контакты", "декорированная функция вернула неверные данные при указании пустого словаря"
+
+
+@Handler(methods=('POST'))
+def index(request):
+    return "index"
+
+
+assert index({"method": "POST"}) == "POST: index", "декорированная функция вернула неверные данные"
+assert index({"method": "GET"}) is None, "декорированная функция вернула неверные данные"
+assert index({"method": "DELETE"}) is None, "декорированная функция вернула неверные данные"
